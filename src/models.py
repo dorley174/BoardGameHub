5d6af32f15165bd6ab00ad09b4ff0eaa23c8d684
KOSMOGOR <mortalmortal2015@mail.ru>
2026-04-05 16:29:58 +0300

    add: other tables

--- src/models.py
+++ src/models.py
@@ -1,6 +1,31 @@
 from sqlmodel import Field, SQLModel
 from typing import Optional
 
+
 class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
+    userId: Optional[int] = Field(default=None, primary_key=True)
+    userName: str = Field(index=True)
+
+
+class Game(SQLModel, table=True):
+    gameId: Optional[int] = Field(default=None, primary_key=True)
+    gameName: str = Field(index=True)
+
+
+class UserGame(SQLModel, table=True):
+    userGameId: Optional[int] = Field(default=None, primary_key=True)
+    userId: int = Field(foreign_key="user.userId")
+    gameId: int = Field(foreign_key="game.gameId")
+    isAvailable: bool = Field(default=True)
+
+
+class Group(SQLModel, table=True):
+    groupId: Optional[int] = Field(default=None, primary_key=True)
+    groupName: str = Field(index=True)
+    creatorUserId: int = Field(foreign_key="user.userId")
+
+
+class GroupMember(SQLModel, table=True):
+    groupMemerId: Optional[int] = Field(default=None, primary_key=True)
+    userId: int = Field(foreign_key="user.userId")
+    groupId: int = Field(foreign_key="group.groupId")
